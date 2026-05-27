8a399ecc36def6f9f1786286b10e5ab0473bbe5f
kishoreeducationgithub <128738882+kishoreeducationgithub@users.noreply.github.com>
2026-05-27 08:56:15 +0000

    kk

--- my-pracise/src/App.jsx
+++ my-pracise/src/App.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import Index from './components/Index'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Index/>
+    </div>
+
   )
 }
 
---
+++ my-pracise/src/components/Index.jsx
@@ -0,0 +1,20 @@
+import "./index.css"
+const Index = () => {
+  return (
+    <div class="bg-[#0f172a] min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-[#f8fafc] text-5xl">
+        Super Over League
+      </h1>
+      <div className="flex flex-row m-2 p-4">
+          <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png
+" alt="kishore" className="w-50 h-50 m-2"/>
+          <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" alt="vyshu" className="w-50 h-50 m-2"/>
+      </div>
+      
+
+    
+    </div>
+  )
+}
+
+export default Index
